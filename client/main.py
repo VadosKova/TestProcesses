ae42d53f8cc39077c5b1a8960d86a05cb302379c
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-03-24 13:07:34 +0200

    Created function for receiving processes

--- client/main.py
+++ client/main.py
@@ -1,3 +1,12 @@
 import socket
 import psutil
 import jsonpickle
+
+def get_processes():
+    process_list = []
+    for proc in psutil.process_iter(['pid', 'name', 'status']):
+        try:
+            process_list.append({'pid': proc.info['pid'], 'name': proc.info['name'], 'status': proc.info['status']})
+        except Exception:
+            pass
+    return process_list
