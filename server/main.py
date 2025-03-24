0ca5ea2e3d179007672777d2c9594995de5177d6
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-03-24 12:58:42 +0200

    Created function for sending data to client

--- server/main.py
+++ server/main.py
@@ -8,3 +8,20 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((IP, PORT))
 server.listen(2)
 print("Сервер запущен...")
+
+
+def process_client(client):
+    try:
+        data = client.recv(1024)
+        if data:
+            processes = jsonpickle.loads(data)
+            print("Result:")
+            for proc in processes:
+                print(f"PID: {proc['pid']} | Name: {proc['name']} | Status: {proc['status']}")
+
+            response = "Data received"
+            client.sendall(response.encode('utf-8'))
+    except Exception:
+        print("Error")
+    finally:
+        client.close()
