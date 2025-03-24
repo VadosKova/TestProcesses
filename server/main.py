89925f46536a45f7145d821ed1d15f0ab64c82b2
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-03-24 12:49:28 +0200

    Connection settings

--- server/main.py
+++ server/main.py
@@ -1,2 +1,10 @@
 import socket
 import jsonpickle
+
+IP = '127.0.0.1'
+PORT = 4000
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((IP, PORT))
+server.listen(2)
+print("Сервер запущен...")
